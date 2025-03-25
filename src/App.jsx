e6d155c53adb25d0a283153182efa7f29580d800
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2025-03-25 19:57:23 +0530

    form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // import Drak from "./Application/Drak Mode/App"
-import Product from "./Application/Product_Search/Product"
+// import Product from "./Application/Product_Search/Product"
+import From from "./Application/Form Validtion/Form"
 
 function App() {
   return (
     <>
-      <Product />
+      <From />
     </>
   )
 }
