51bde277fb08ae5147d0887beb6bffa4d9315460
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-04-02 17:31:00 +0530

    imgae input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 // import Drak from "./Application/Drak Mode/App"
 // import Product from "./Application/Product_Search/Product"
-import From from "./Application/Form Validtion/Form"
+// import From from "./Application/Form Validtion/Form"
+import Image_input from "./Application/Input Image/Image/Image";
+// import PDF_input from "./Application/Input Image/File/File";
 
 function App() {
   return (
     <>
-      <From />
+      <Image_input />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
