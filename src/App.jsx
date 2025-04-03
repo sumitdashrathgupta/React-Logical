9f3423c8c22a31d1ab70881db8bf4b27e2cc2060
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-04-03 13:41:28 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import Drak from "./Application/Drak Mode/App"
-// import Product from "./Application/Product_Search/Product"
+import Product from "./Application/Product_Search/Product"
 // import From from "./Application/Form Validtion/Form"
-import Image_input from "./Application/Input Image/Image/Image";
+// import Image_input from "./Application/Input Image/Image/Image";
 // import PDF_input from "./Application/Input Image/File/File";
 
 function App() {
   return (
     <>
-      <Image_input />
+      <Product />
     </>
   );
 }
